523397812a11b601b0a5d7ed6376ab39176750ee
Jheelam <jheelamhossain147@gmail.com>
2025-11-17 01:20:12 +0530

    Implement translation using MarianMT model

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -1,2 +1,10 @@
+from transformers import MarianMTModel, MarianTokenizer
+
+model_name = "Helsinki-NLP/opus-mt-en-hi"
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+
 def baseline_translate(text):
-    return text[::-1]  # reverse text to simulate translation
+    inputs = tokenizer(text, return_tensors="pt", padding=True)
+    translated = model.generate(**inputs)
+    return tokenizer.decode(translated[0], skip_special_tokens=True)
