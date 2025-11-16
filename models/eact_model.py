c3c3921845f42b130eafbef2ea7ef670b3fabea1
Jheelam <jheelamhossain147@gmail.com>
2025-11-17 01:20:24 +0530

    Integrate MarianMT for EACT translation

--- models/eact_model.py
+++ models/eact_model.py
@@ -1,2 +1,15 @@
+from transformers import MarianMTModel, MarianTokenizer
+
+model_name = "Helsinki-NLP/opus-mt-en-hi"
+tokenizer = MarianTokenizer.from_pretrained(model_name)
+model = MarianMTModel.from_pretrained(model_name)
+
 def eact_translate(text):
-    return "EACT translation for: " + text
+    inputs = tokenizer(text, return_tensors="pt", padding=True)
+    translated = model.generate(
+        **inputs,
+        num_beams=6,          # more robust decoding
+        length_penalty=1.2,
+        early_stopping=True
+    )
+    return tokenizer.decode(translated[0], skip_special_tokens=True)
